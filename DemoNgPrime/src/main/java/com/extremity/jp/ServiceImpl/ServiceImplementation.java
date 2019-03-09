72779a2596227bff1b6415bc2fd124149e5a90e7
vyankteshaje <vyankteshaje@gmail.com>
2019-03-09 16:03:03 +0530

    Hello commit with controller and ServiceImplementation

--- DemoNgPrime/src/main/java/com/extremity/jp/ServiceImpl/ServiceImplementation.java
+++ DemoNgPrime/src/main/java/com/extremity/jp/ServiceImpl/ServiceImplementation.java
@@ -1,9 +1,45 @@
 package com.extremity.jp.ServiceImpl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.extremity.jp.Model.Candidate;
+import com.extremity.jp.ServiceI.ServiceInterface;
+import com.extremity.jp.repository.CandidateRepository;
+
 @Repository
-public class ServiceImplementation {
+public class ServiceImplementation implements ServiceInterface {
+
+	@Autowired
+	CandidateRepository cr;
+	
+	
+	@Override
+	public List<Candidate> getAllcandidate() {
+		// TODO Auto-generated method stub
+		List<Candidate> list=(List<Candidate>) cr.findAll();
+		
+		return list;
+	}
+
+	@Override
+	public String saveCandidate(Candidate c) {
+		// TODO Auto-generated method stub
+		cr.save(c);
+		return "save candidate Successfully";
+	}
+
+	@Override
+	public String delete(int id) {
+		// TODO Auto-generated method stub
+		cr.deleteById(id);
+		return "Deleted Candidate Successfully";
+	}
+	
+	
+	
 
 	
 	
--- DemoNgPrime/src/main/java/com/extremity/jp/controller/HomeController.java
+++ DemoNgPrime/src/main/java/com/extremity/jp/controller/HomeController.java
@@ -1,9 +1,41 @@
 package com.extremity.jp.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.extremity.jp.Model.Candidate;
+import com.extremity.jp.ServiceI.ServiceInterface;
 
 @RestController
 public class HomeController {
+	
+	@Autowired
+	ServiceInterface si;
+	
+	@RequestMapping(value="/getAllcandidate",method=RequestMethod.GET)
+	public List<Candidate> getAllCandidate(){
+		
+		
+		List<Candidate> list=	si.getAllcandidate();
+		
+		return list;
+		
+	}
+	
+	@RequestMapping(value="/saveCandidate",method=RequestMethod.POST)
+	public String saveCandidate(@RequestBody Candidate c) {
+	
+		si.saveCandidate(c);
+		
+		return "Save successfully";
+		
+	}
+	
 	
 	
 
